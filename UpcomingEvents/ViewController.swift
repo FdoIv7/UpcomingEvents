dc7338e1140ec11c3d54583ad76a1f1cd044f0c4
Fernando Perez <fdoiv7@gmail.com>
2021-11-29 21:35:28 -0600

    Initial Commit

---
+++ UpcomingEvents/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UpcomingEvents
+//
+//  Created by Fernando Pérez Ruiz on 29/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
